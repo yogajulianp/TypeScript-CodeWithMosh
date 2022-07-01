5d76ab39a2aa2e346a900a657f6f7cec5db93c21
yoga.julian <yoga.julian@kodingworks.io>
2022-07-02 06:29:47 +0700

    Advace Types , Classes, Interface and OOP

---
+++ 3-Advanced Types/hello-world/dist/exerciseAdvance.js
@@ -0,0 +1,13 @@
+"use strict";
+let users = [
+    {
+        name: 'John Smith',
+        age: 30,
+        occupation: 'Software engineer'
+    },
+    {
+        name: 'Kate Müller',
+        age: 28
+    }
+];
+//# sourceMappingURL=exerciseAdvance.js.map
